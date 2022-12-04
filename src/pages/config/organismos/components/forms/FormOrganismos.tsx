beaf1d147afe879d6019cf6beaf9d50f1ce9d7f4
Yanet Brito <yabf0801@gmail.com>
2022-12-03 19:25:03 -0500

    add org form

--- src/pages/config/organismos/components/forms/FormOrganismos.tsx
+++ src/pages/config/organismos/components/forms/FormOrganismos.tsx
@@ -10,111 +10,73 @@ type FormOrganismosProps = {
 const FormOrganismos: React.FC<FormOrganismosProps> = ({ values }) => {
   return (
      
-          <div className="container ">
-            <div className="card-header "> 
-              <h3 className="text-center">Nuevo Organismo</h3>
+    <section className="container">
+      <div className="container">
+      <div className="row d-flex justify-content-center align-items-center ">
+        <div className="col">
+          <div className="card-body organismo text-left gap-3">
+            <div className="mb-md-5 mt-md-4">
+              <div className="card-body text-black">
+                <h3 className="text-dark d-flex justify-content-center align-items-center">Nuevo Organismo</h3>
+
+                <div className="row g-3 justify-content-between align-items-center">
+                  <div className="col-sm-4">
+                  <div className="form-outline">
+                      <label className="form-label" htmlFor="name">
+                        Nombre
+                      </label>
+                      <Field
+                        className="form-control"
+                        type="text"
+                        name="organismos.name"
+                        id="name"
+                        value={values.name}
+                      />
+                    </div>
+                    <ErrorMessage name="organismos.name" component="p" className="text-danger" />
+                    </div>
+
+                  <div className="col-sm-6">
+                    <div className="form-outline">
+                      <label className="form-label" htmlFor="description">
+                        Descripcion
+                      </label>
+                      <Field
+                        type="text"
+                        className="form-control"
+                        name="organismos.description"
+                        id="description"
+                        value={values.description}
+                      />
+                    </div>
+                    <ErrorMessage name="organismos.description" component="p" className="text-danger" />
+                    </div>
+
+                    <div className="col-sm-2 p-lg-5">
+                    <div className="form-check form-switch">
+                      <Field
+                        className="form-check-input switch-lg"
+                        type="checkbox"
+                        role="switch"
+                        name="organismos.priorizado"
+                        id="priorizado"
+                        checked={values.priorizado}
+                      />
+                      <label className="form-label" htmlFor="priorizado">
+                        Priorizado
+                      </label>
+                    </div>
+                  </div>
+
+                  </div>
+                </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-             <div className="card-body d-flex gap-3">
-              <label className="form-label ">
-                No.
-                <Field
-                  className="form-control"
-                  type="number"
-                  name="no"
-                  id="no"
-                  placeholder="#"
-                  value={values.no}
-                />
-                <ErrorMessage name="no" component="p" className="text-danger" />
-                </label>
-
-              <label className="form-label">
-                Nombre del circulo
-                <Field
-                  className="form-control"
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder=" Nombre del circulo"
-                  value={values.name}
-                />
-                <ErrorMessage name="name" component="p" className="text-danger" />
-              </label>
-
-              
-            </div>
-
-            <div className="col-sm-4 d-flex flex-column flex-md-row gap-3">
-            <label className="form-label col-sm-4 ">
-                Capacidad 2do
-                <Field
-                  className="form-control"
-                  type="number"
-                  name="normed_capacity2"
-                  id="normed_capacity2"
-                  placeholder=" #"
-                  value={values.normed_capacity2}
-                />
-                <ErrorMessage name="normed_capacity2" component="p" className="text-danger" />
-              </label>
-
-              <label className="form-label col-sm-4 ">
-                Capacidad 3ro
-                <Field
-                  className="form-control"
-                  type="number"
-                  name="normed_capacity3"
-                  id="normed_capacity3"
-                  placeholder=" #"
-                  value={values.normed_capacity3}
-                />
-                <ErrorMessage name="normed_capacity3" component="p" className="text-danger" />
-              </label>
-
-              <label className="form-label col-sm-4 ">
-                Capacidad 4to
-                <Field
-                  className="form-control"
-                  type="number"
-                  name="normed_capacity4"
-                  id="normed_capacity4"
-                  placeholder=" #"
-                  value={values.normed_capacity4}
-                />
-                <ErrorMessage name="normed_capacity4" component="p" className="text-danger" />
-              </label>
-
-              <label className="form-label col-sm-4 ">
-                Capacidad 5to
-                <Field
-                  className="form-control"
-                  type="number"
-                  name="normed_capacity5"
-                  id="normed_capacity5"
-                  placeholder=" #"
-                  value={values.normed_capacity5}
-                />
-                <ErrorMessage name="normed_capacity5" component="p" className="text-danger" />
-              </label>
-
-              <label className="form-label col-sm-4 ">
-                Capacidad 6to
-                <Field
-                  className="form-control"
-                  type="number"
-                  name="normed_capacity6"
-                  id="normed_capacity6"
-                  placeholder=" #"
-                  value={values.normed_capacity6}
-                />
-                <ErrorMessage name="normed_capacity6" component="p" className="text-danger" />
-              </label>
-
-            </div>
-           </div>
-
-    
+           </section>   
 );
 };
 
